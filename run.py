3fa5eed4f099ef55d2e875326afed4ec0fe4b3cf
liuliansuchengjing <1400325886@qq.com>
2026-01-03 11:19:09 +0800

    model修改

--- run.py
+++ run.py
@@ -127,7 +127,7 @@ def train_model(MSHGAT, data_path):
     opt.user_size = user_size
 
     # ========= Preparing Model =========#
-    model = MSHGAT(opt, dropout=opt.dropout)
+    model = MSHGAT(user_size, opt, dropout=opt.dropout)
     loss_func = nn.CrossEntropyLoss(size_average=False, ignore_index=Constants.PAD)
     kt_loss = KTLoss()
 
@@ -242,7 +242,7 @@ def test_model(MSHGAT, data_path):
 
     opt.user_size = user_size
 
-    model = MSHGAT(opt, dropout=opt.dropout)
+    model = MSHGAT(user_size, opt, dropout=opt.dropout)
     model.load_state_dict(torch.load(opt.save_path))
     model.cuda()
     kt_loss = kt_loss.cuda()
